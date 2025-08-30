9f561a7125011e9d9ef7de4614343b1cd5588223
cyberdev369 <tanuga_max@hotmail.com>
2025-08-30 00:41:18 -0400

    english text
    actualizacion de textos y score

--- Module1_Intro_Python/4-Translate_Date.py
+++ Module1_Intro_Python/4-Translate_Date.py
@@ -1,15 +1,27 @@
-name = input("¿Cuál es tu nombre? ")
-first_name = input("¿Cuál es tu apellido? ")
-print("Hellow", name.capitalize()+ " " + first_name.capitalize(), "¡Bienvenido a Python!")
+name = input("What are you name? ")
+first_name = input("waht are your first name? ")
+print("Hellow", name.capitalize()+ " " + first_name.capitalize(), "Welcome to the python curse of the honeycomb!")
 
-adulto = str(input("¿Eres un adulto? (si/no) "))
-r_major = "Tu eres un adulto, puedes acceder! "
-r_menor = "Eres menor de edad, no puedes acceder! "
-if adulto == "si":
- date = int(input("¿Que edad tienes? "))
+adulto = str(input("are you an adult (yes/no) "))
+r_major = "you are major, you can enter! "
+r_menor = "you are minor, you can't enter! "
+if adulto == "yes":
+ date = int(input("What is date? "))
  if date >= 18:
   print (r_major)
- else:
+ else :
   print (r_menor)
 elif adulto == "no":
   print (r_menor)
+
+score = date
+if score >= 18 and score <= 24:
+    print("Grade: A")
+elif score >= 25:
+    print("Grade: B")
+elif score >= 35:
+    print("Grade: C")
+elif score >= 45:
+    print("Grade: D") 
+else: 
+    print("Grade: X")
